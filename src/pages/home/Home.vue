bbab6e92f7255172b94321232dc4a7711ca55a8c
wanwanlo <18735161363@163.com>
2017-11-01 17:11:54 +0800

    'change'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+ 	<home-header>header</home-header>
+</template>
+
+<script>
+import HeaderComponent from "./Header"
+export default{
+	components:{
+		"home-header":HeaderComponent
+	}
+}
+</script>
+
+<style>
+	
+</style>
